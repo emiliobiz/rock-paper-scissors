df1e38853a9f845295a71efe243a6f5f2275cab3
Emilio Biz <151553891+emiliobiz@users.noreply.github.com>
2024-02-01 11:21:46 +0100

    Add user choice prompt & if/else game logic

--- script.js
+++ script.js
@@ -4,10 +4,41 @@
 // Output the result 
 // Prompt to start again - back to step 1
 
-const gameOptions = ['Rock', 'Paper', 'Scissors']
+const gameOptions = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice(){
     let randomIndex = Math.floor(Math.random() * gameOptions.length);
     let computerChoice = gameOptions[randomIndex];
     return computerChoice
-}
+};
+
+function getUserChoice (){
+    let userChoice = prompt("Choose between: Rock, paper or scissors")
+    return userChoice.toLowerCase()
+};
+
+function compareChoices(computerSelection, userSelection){
+    if (computerSelection == userSelection){
+        return 'Tie!'
+    }
+
+    else if (computerSelection == 'rock' && userSelection == 'scissors'){
+        return 'You lost! Rock wins scissors!'
+    }
+
+    else if (computerSelection == 'paper' && userSelection == 'rock'){
+        return 'You lost! Paper wins rock!'
+    }
+
+    else if (computerSelection == 'scissors' && userSelection == 'paper'){
+        return 'You lost! Scissors wins paper!'
+    }
+
+    else {return 'You won!'}
+    
+};
+
+const computerSelection = getComputerChoice();
+const userSelection = getUserChoice();
+
+console.log(compareChoices(computerSelection, userSelection))
